0ff971425f01c4b70e51c715cd0877953213efc4
Merdan <163035166+Merdan080401@users.noreply.github.com>
2024-05-16 21:41:19 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,67 @@
 // Copyright 2022 UNN-IASR
+
 #include "fun.h"
+#include <cctype>
+#include <cmath>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+
+    while (*str) {
+        if (isspace(*str)) {
+            inWord = false;
+        } else {
+            if (!isdigit(*str) && !inWord) {
+                ++count;
+            }
+            inWord = true;
+        }
+        ++str;
+    }
+
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+
+    while (*str) {
+        if (isspace(*str)) {
+            inWord = false;
+        } else {
+            if (isupper(*str) && !inWord) {
+                ++count;
+            } else if (inWord && !islower(*str)) {
+                inWord = false;
+            }
+            inWord = true;
+        }
+        ++str;
+    }
+
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int sumLength = 0;
+    unsigned int wordCount = 0;
+    bool inWord = false;
+
+    while (*str) {
+        if (isspace(*str)) {
+            inWord = false;
+        } else {
+            if (!inWord) {
+                ++wordCount;
+            }
+            ++sumLength;
+            inWord = true;
+        }
+        ++str;
+    }
+
+    return (sumLength + wordCount - 1) / wordCount;
 }
+
