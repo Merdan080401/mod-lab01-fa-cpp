5256c9c9915ce5c2bd0303aa4ab0dfdcf3b85015
Merdan <163035166+Merdan080401@users.noreply.github.com>
2024-05-16 23:44:29 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,18 +4,48 @@
 
 unsigned int faStr1(const char *str) {
     unsigned int count = 0;
+    const char *wordStart = nullptr;
     bool inWord = false;
 
     while (*str) {
-        if (isspace(*str)) {
-            inWord = false;
-        } else {
-            if (!isdigit(*str) && !inWord) {
-                ++count;
+        if (std::isspace(*str)) {
+            if (inWord) {
+                inWord = false;
+                const char *ptr = wordStart;
+                bool hasDigit = false;
+                while (ptr < str) {
+                    if (std::isdigit(*ptr)) {
+                        hasDigit = true;
+                        break;
+                    }
+                    ptr++;
+                }
+                if (!hasDigit) {
+                    count++;
+                }
             }
-            inWord = true;
+        } else {
+            if (!inWord) {
+                inWord = true;
+                wordStart = str;
+            }
         }
-        ++str;
+        str++;
+    }
+
+    if (inWord) {
+        const char *ptr = wordStart;
+        bool hasDigit = false;
+        while (*ptr) {
+            if (std::isdigit(*ptr)) {
+                hasDigit = true;
+                break;
+            }
+            ptr++;
+        }
+        if (!hasDigit) {
+            count++;
+        }
     }
 
     return count;
@@ -24,19 +54,49 @@ unsigned int faStr1(const char *str) {
 unsigned int faStr2(const char *str) {
     unsigned int count = 0;
     bool inWord = false;
+    bool startUppercase = false;
 
     while (*str) {
-        if (isspace(*str)) {
-            inWord = false;
-        } else {
-            if (isupper(*str) && !inWord) {
-                ++count;
-            } else if (inWord && !islower(*str)) {
+        if (std::isspace(*str)) {
+            if (inWord) {
                 inWord = false;
+                if (startUppercase) {
+                    bool onlyLowercase = true;
+                    const char *ptr = str - 1;
+                    while (ptr >= str - 1 - count) {
+                        if (!std::islower(*ptr)) {
+                            onlyLowercase = false;
+                            break;
+                        }
+                        ptr--;
+                    }
+                    if (onlyLowercase) {
+                        count++;
+                    }
+                }
             }
-            inWord = true;
+        } else {
+            if (!inWord) {
+                inWord = true;
+                startUppercase = std::isupper(*str);
+            }
         }
-        ++str;
+        str++;
+    }
+
+    if (inWord && startUppercase) {
+        bool onlyLowercase = true;
+        const char *ptr = str - 1;
+        while (ptr >= str - 1 - count) {
+            if (!std::islower(*ptr)) {
+                onlyLowercase = false;
+                break;
+            }
+            ptr--;
+        }
+        if (onlyLowercase) {
+            count++;
+        }
     }
 
     return count;
