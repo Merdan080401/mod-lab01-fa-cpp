47ab008199e86bd21821c6cea656f5bde11bdad2
Merdan <163035166+Merdan080401@users.noreply.github.com>
2024-05-16 21:42:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 // Copyright 2022 UNN-IASR
+
+#include <iostream>
 #include "fun.h"
 
 int main() {
-    //fsfrgergerger
+    const char *str = "Hello World 123Hello hello world world";
+    
+    std::cout << "Задание 1: " << faStr1(str) << std::endl;
+    std::cout << "Задание 2: " << faStr2(str) << std::endl;
+    std::cout << "Задание 3: " << faStr3(str) << std::endl;
+
     return 0;
 }
