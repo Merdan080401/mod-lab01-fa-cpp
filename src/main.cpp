8021116ea150e174f2599f70354ebbf725981c4d
Merdan <163035166+Merdan080401@users.noreply.github.com>
2024-05-17 21:30:24 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "fun.h"
+
+int main() {
+    const char* str = "Hello World 123Hello hello world world";
+
+    std::cout << "Задание 1: " << faStr1(str) << std::endl;
+    std::cout << "Задание 2: " << faStr2(str) << std::endl;
+    std::cout << "Задание 3: " << faStr3(str) << std::endl;
+
+    return 0;
+}
